c703cf7baa2fe3c66246cfd00688fbc9d2429641
sneoldum <neoldums@gmail.com>
2023-05-20 00:07:40 +0300

    Implemented Alpha Vantage Api

---
+++ StockSavvy/Pages/Stock.cshtml.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using StockSavvy.Services;
+using static StockSavvy.Models.StockModel;
+
+namespace StockSavvy.Pages
+{
+    public class StockModel : PageModel
+    {
+        private readonly ILogger<StockModel> _logger;
+
+        public StockModel(ILogger<StockModel> logger)
+        {
+            _logger = logger;
+        }
+        [BindProperty]
+        public string StockCode { get; set; }
+
+        [BindProperty]
+        public string DataOpen { get; set; }
+
+        [BindProperty]
+        public string DataClose { get; set; }
+
+        [BindProperty]
+        public string DataHigh { get; set; }
+
+        [BindProperty]
+        public string DataLow { get; set; }
+
+        [BindProperty]
+        public string DataVolume { get; set; }
+
+        [BindProperty]
+        public string DataSymbol { get; set; }
+
+        [BindProperty]
+        public string DataLast { get; set; }
+
+        [BindProperty]
+        public List<Data> DataList { get; set; }
+        [BindProperty]
+        public List<string> Date { get; set; }
+
+        [BindProperty]
+        public string key { get; set; }
+
+
+        public void OnPostStockRequest(StockService stockService)
+
+        {
+            Console.WriteLine(StockCode);
+            var stockModel = stockService.GetStockModel(StockCode,key);
+            if (stockModel != null)
+            {
+                var firstOrDefaultValue = stockModel.Datas.Values.FirstOrDefault();
+                Date = stockModel.Datas.Keys.ToList();
+                DataList =  stockModel.Datas.Values.ToList();
+                
+                DataSymbol = stockModel.metaData.Symbol;
+                DataLast = stockModel.metaData.LastRefreshed;
+                DataOpen = firstOrDefaultValue.Open;
+                DataClose = firstOrDefaultValue.Close;
+                DataHigh = firstOrDefaultValue.High;
+                DataLow = firstOrDefaultValue.Low;
+                DataVolume = firstOrDefaultValue.Volume;
+            }
+
+        }
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ StockSavvy/Services/StockService.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using System.Net;
+using Newtonsoft.Json;
+using StockSavvy.Models;
+using static StockSavvy.Models.StockModel;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+
+namespace StockSavvy.Services
+{
+    public class StockService
+    {
+
+        public StockModel? GetStockModel(string stockCode,string apikey)
+        {
+            if (apikey == null)
+            {
+                apikey = "demo"; // Your api key 
+                stockCode = "IBM";
+            }
+            string url = string.Concat("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + stockCode + "&interval=5min&apikey=" + apikey);
+
+
+            //string url = string.Concat("https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=" + stockCode+"&apikey=", apikey);
+
+            var json = new WebClient().DownloadString(url);
+            StockModel? stockData = JsonSerializer.Deserialize<StockModel>(json);
+            
+            Console.WriteLine(stockData.Datas);
+            return stockData;
+
+        }
+    }
+}
