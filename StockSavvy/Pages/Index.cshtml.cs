ab92cdafb3d4a7357c4e16c9b5544297e92cf9d8
Yusuf AĞAÇ <74566648+Yusuf-Agac@users.noreply.github.com>
2023-05-19 15:02:08 +0300

    feat: listing cryptos in top 100 by volume

--- StockSavvy/Pages/Index.cshtml.cs
+++ StockSavvy/Pages/Index.cshtml.cs
@@ -1,20 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Binance.Net;
+using Binance.Net.Clients;
+using System.Linq;
+using Binance.Net.Interfaces;
 
 namespace StockSavvy.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly BinanceClient binanceClient;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel()
         {
-            _logger = logger;
+            binanceClient = new BinanceClient(); // Replace apiKey and apiSecret with your actual Binance API credentials
         }
+        
+        public List<IBinanceTick> tickers { get; private set; }
 
-        public void OnGet()
+        public async Task OnGet()
         {
-
+            var spotTickerData = await binanceClient.SpotApi.ExchangeData.GetTickersAsync();
+            tickers = spotTickerData.Data.Where(ticker => ticker.Symbol.EndsWith("USDT")).OrderByDescending(ticker => ticker.Volume * ticker.LastPrice).Take(100).ToList();
         }
     }
 }
