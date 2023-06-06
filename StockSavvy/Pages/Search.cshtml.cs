8b154dbd9a96c2a90d1fc915cd6396ae575b2cdd
Hakan <hakantekir2002@outlook.com>
2023-06-06 19:27:31 +0300

    feat: Create search page

---
+++ StockSavvy/Pages/Search.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StockSavvy.Pages
+{
+
+    public class SearchModel : PageModel
+    {
+        [BindProperty(SupportsGet = true)]
+        public string Query { get; set; }
+
+        public List<string> Results { get; set; } = new List<string>();
+
+        public void OnGet()
+        {
+            if (!string.IsNullOrEmpty(Query))
+            {
+                // TODO: Get stock data from API
+                // TODO: Draw chart
+                // TODO: Add button named "Buy" and "Sell"
+            }
+        }
+    }
+}
